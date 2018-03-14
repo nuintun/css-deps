ecb99abbfcb74bfea2d768d5f46e1727cd4edead
nuintun <nuintun@qq.com>
2018-03-14 16:11:52 +0800

    :art: Rewrite

--- tests/test.js
+++ tests/test.js
@@ -2,15 +2,17 @@ const css = require('../dist/index.src');
 
 const code = `
 @charset "utf-8";
+
 @import "example1.css";
 @import url(example2.css);
 @import url("example3.css");
-@import "example4.css" screen and (min-width:800px);
-@import url(example5.css) screen and (min-width:800px);
-@import url(example6.css) screen and (width:800px), (color);
-@import url(example7.css) screen and (min-device-width:500px) and (max-device-width:1024px);
+@import "example4.css" screen and (min-width: 800px);
+@import url(example5.css) screen and (min-width: 800px);
+@import url(example6.css) screen and (width: 800px), (color);
+@import url(example7.css) screen and (min-device-width: 500px) and (max-device-width: 1024px);
 
-:root .fg, .bg{
+:root .fg,
+.bg {
   -moz-background-image: image-set(url(img/test.png) 1x, url("img/test-2x.png") 2x, url(my-img-print.png) 600dpi);
   -ms-filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src="rain1977.gif", sizingMethod="scale");
 }
