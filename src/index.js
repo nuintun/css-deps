332d517d783f2783f694197c7dccecbf30519c2e
nuintun <nuintun@qq.com>
2018-04-18 10:27:01 +0800

    :art: Rewrite

---
+++ src/index.js
@@ -0,0 +1,71 @@
+/**
+ * @module index
+ * @license MIT
+ * @version 2017/11/10
+ */
+
+import * as postcss from 'postcss';
+import * as utils from './lib/utils';
+import parseImport from './lib/parse-import';
+import parseAssets from './lib/parse-assets';
+
+/**
+ * @function parser
+ * @param {string|Buffer} code
+ * @param {Function} [replace]
+ * @param {Object} [options]
+ * @param {Object} [options.postcss]
+ * @param {Function} [options.onpath]
+ * @returns {Object}
+ */
+export default function parser(code, replace, options) {
+  let syntax;
+  const dependencies = [];
+
+  // Is buffer
+  if (Buffer.isBuffer(code)) code = code.toString();
+
+  if (replace && utils.object(replace)) {
+    options = replace;
+    replace = null;
+  }
+
+  options = options || {};
+
+  try {
+    syntax = postcss.parse(code, options.postcss);
+  } catch (error) {
+    return { code, dependencies };
+  }
+
+  if (replace && !utils.fn(replace)) replace = null;
+
+  const onpath = utils.fn(options.onpath) ? options.onpath : null;
+
+  syntax.walk(node => {
+    switch (node.type) {
+      // At rule
+      case 'atrule':
+        if (node.name === 'import') {
+          const parsed = parseImport(node, replace, options);
+          const code = parsed.code;
+          const path = parsed.path;
+          const media = parsed.media;
+
+          dependencies.push({ path, media });
+          code ? (node.params = code) : node.remove();
+        }
+        break;
+      // Declaration
+      case 'decl':
+        parseAssets(node, onpath);
+        break;
+    }
+  });
+
+  // Get css code
+  code = syntax.toResult().css;
+
+  // Returned
+  return { code, dependencies };
+}
